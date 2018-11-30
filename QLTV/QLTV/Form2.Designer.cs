3c3b9a8a464778ac83907a6cb28fbbcd7ccf73cc
Thanh829 <42971407+Thanh829@users.noreply.github.com>
2018-11-30 22:35:44 +0700

    new

---
+++ QLTV/QLTV/Form2.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace QLTV
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ten = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sdt = new System.Windows.Forms.TextBox();
+            this.dc = new System.Windows.Forms.TextBox();
+            this.ms = new System.Windows.Forms.TextBox();
+            this.ngaydk = new System.Windows.Forms.TextBox();
+            this.date = new System.Windows.Forms.TextBox();
+            this.mail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ok = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ten
+            // 
+            this.ten.AllowDrop = true;
+            this.ten.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ten.Location = new System.Drawing.Point(153, 206);
+            this.ten.Name = "ten";
+            this.ten.Size = new System.Drawing.Size(252, 31);
+            this.ten.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-27, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Họ Tên";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // sdt
+            // 
+            this.sdt.AllowDrop = true;
+            this.sdt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sdt.Location = new System.Drawing.Point(153, 471);
+            this.sdt.Name = "sdt";
+            this.sdt.Size = new System.Drawing.Size(252, 31);
+            this.sdt.TabIndex = 2;
+            // 
+            // dc
+            // 
+            this.dc.AllowDrop = true;
+            this.dc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dc.Location = new System.Drawing.Point(153, 377);
+            this.dc.Name = "dc";
+            this.dc.Size = new System.Drawing.Size(252, 31);
+            this.dc.TabIndex = 3;
+            // 
+            // ms
+            // 
+            this.ms.AllowDrop = true;
+            this.ms.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ms.Location = new System.Drawing.Point(153, 294);
+            this.ms.Name = "ms";
+            this.ms.Size = new System.Drawing.Size(252, 31);
+            this.ms.TabIndex = 4;
+            // 
+            // ngaydk
+            // 
+            this.ngaydk.AllowDrop = true;
+            this.ngaydk.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ngaydk.Location = new System.Drawing.Point(655, 377);
+            this.ngaydk.Name = "ngaydk";
+            this.ngaydk.Size = new System.Drawing.Size(231, 31);
+            this.ngaydk.TabIndex = 5;
+            // 
+            // date
+            // 
+            this.date.AllowDrop = true;
+            this.date.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date.Location = new System.Drawing.Point(655, 294);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(231, 31);
+            this.date.TabIndex = 6;
+            // 
+            // mail
+            // 
+            this.mail.AllowDrop = true;
+            this.mail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mail.Location = new System.Drawing.Point(655, 206);
+            this.mail.Name = "mail";
+            this.mail.Size = new System.Drawing.Size(231, 31);
+            this.mail.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(464, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ngày lập thẻ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(464, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ngày sinh";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(464, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 31);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Email";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(-27, 471);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 31);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Số ĐT";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(-27, 377);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 31);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Địa chỉ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(-27, 294);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(155, 31);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Mã độc giả";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // ok
+            // 
+            this.ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ok.Location = new System.Drawing.Point(643, 460);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(117, 38);
+            this.ok.TabIndex = 15;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(384, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 40);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Đăng Kí";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 539);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.mail);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.ngaydk);
+            this.Controls.Add(this.ms);
+            this.Controls.Add(this.dc);
+            this.Controls.Add(this.sdt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ten);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ten;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sdt;
+        private System.Windows.Forms.TextBox dc;
+        private System.Windows.Forms.TextBox ms;
+        private System.Windows.Forms.TextBox ngaydk;
+        private System.Windows.Forms.TextBox date;
+        private System.Windows.Forms.TextBox mail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ QLTV/QLTV/ThamKhao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV
+{
+   public class ThamKhao : Sach
+    {
+        public string Quocgia;
+        public ThamKhao() : base()
+        {
+
+        }
+        public void nhap(string masach, string tua, string nxb, string tacgia, int gia, string quocgia,int soluong)
+        {
+            base.nhap(masach, tua, nxb, tacgia, gia,soluong);
+            this.Quocgia = quocgia;
+        }
+    }
+}
